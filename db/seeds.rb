ed90aa42164284d0cfcdc6c1f82921aac7b9274e
Akilah Snowden <akilahdsnowden@gmail.com>
2022-06-21 17:40:15 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,30 @@
+puts "Creating Strain"
+s1 = Strain.create(type: "Indica")
+s2 = Strain.create(type: "Sativa")
+s3 = Strain.create(type: "Hybrid")
+
+
+
+puts "Creating Item"
+Item.create(name: "Granddaddy Purple", image: "", description: "", strain_id: s1.id , review_id: )
+Item.create(name: "Forbidden Fruit", image: "", description: "", strain_id: s1.id, review_id: )
+Item.create(name: "Yoda OG", image: "", description: "", strain_id: s1.id, review_id: )
+Item.create(name: "Sour Diesel", image: "", description: "", strain_id: s2.id, review_id: )
+Item.create(name: "Green Crack", image: "", description: "", strain_id: s2.id, review_id: )
+Item.create(name: "Mauai Wowie", image: "", description: "", strain_id: s2.id, review_id: )
+Item.create(name: "Dosidos", image: "", description: "", strain_id: s3.id, review_id: )
+Item.create(name: "Mimosa", image: "", description: "", strain_id: s3.id, review_id: )
+Item.create(name: "Wonka Bars", image: "", description: "", strain_id: s3.id, review_id: )
+
+puts "Creating Reviews"
+r1 = Review.create(review: "")
+r2 = Review.create(review: "")
+r3 = Review.create(review: "")
+r4 = Review.create(review: "")
+r5 = Review.create(review: "")
+r6 = Review.create(review: "")
+r7 = Review.create(review: "")
+r8 = Review.create(review: "")
+r9 = Review.create(review: "")
+
 
